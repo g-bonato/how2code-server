6717e01c9768d3ea6c1718002c3778401ab6f1e7
Gabriel Bonato <gabriel.bonato@outlook.com>
2019-07-29 20:46:07 -0300

    Add cookies for session controller

--- src/main/java/com/recommendersystem/recommender/repository/UserRepository.java
+++ src/main/java/com/recommendersystem/recommender/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.recommendersystem.recommender.models.User;
 
@@ -11,6 +12,9 @@ public interface UserRepository extends MongoRepository<User, String> {
 	public List<User> findByEmail(String email);
 
 	public List<User> findByEmailAndPassword(String email, String password);
+
+	@Query("{ 'session' : { 'sessionId' : ?0 } }")
+	public Optional<User> findBySessionId(String sessionId);
 
 	@Override
 	public Optional<User> findById(String id);
